761d3274889af0c694fcc6872a932b8f166ae721
venkateshvipparthi <venkateshsai.vipparthi@gmail.com>
2022-03-10 01:13:33 -0800

    Initial Commit

---
+++ iOSUITableViewProgramatical/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSUITableViewProgramatical
+//
+//  Created by Admin on 10/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
